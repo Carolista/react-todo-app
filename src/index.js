fe2abdb4ff2d1787b790dfb23907c2a9b3408b65
Carolista <cjones.renaissancewoman@gmail.com>
2021-04-30 19:32:55 -0500

    built

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Libraries
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 
 // Components
 import TodoContainer from "./functionBased/components/TodoContainer"
